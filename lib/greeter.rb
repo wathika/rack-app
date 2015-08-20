3b70c4b277aebebda05462d5f93291f81c161d91
wathika <vcwathika@gmail.com>
2015-08-20 17:52:06 +0300

    turned request into an instance variable

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -4,12 +4,12 @@ class Greeter
   def call(env)
     #[200, {"Content-Type" => "text/plain"}, ["Hello rack"]]
     #Rack::Response.new("Hello")
-    request = Rack::Request.new(env)
-    case request.path #to access the url path that the user typed in  to access this page
+    @request = Rack::Request.new(env)
+    case @request.path #to access the url path that the user typed in  to access this page
     when "/" then Rack::Response.new(render("index.html.erb"))
     when "/change" #setting a cookie and redirecting back to the root homepage
        Rack::Response.new do |response|
-         response.set_cookie("greet", request.params["name"])
+         response.set_cookie("greet", @request.params["name"])
          response.redirect("/")
        end
     else Rack::Response.new("Not Found", 404)
@@ -21,7 +21,7 @@ def render(template)
 end
 
 def greet_name  #setting it to the value of the cookie
-  request.cookies["greet"] || "world" 
+  @request.cookies["greet"] || "world"
 
 end
 

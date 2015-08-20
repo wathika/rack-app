7a778154e175bb77951b8dc8f0d92ce85f61d27d
wathika <vcwathika@gmail.com>
2015-08-20 16:34:33 +0300

    rendered html template into my rack app

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,7 +1,14 @@
+require "erb"
+
 class Greeter
   def call(env)
     #[200, {"Content-Type" => "text/plain"}, ["Hello rack"]]
-    Rack::Response.new("Hello")
+    #Rack::Response.new("Hello")
+    Rack::Response.new(render("index.html.erb"))
   end
+def render(template)
+  path = File.expand_path("../views/#{template}", __FILE__)
+  ERB.new(File.read(path)).result(binding)
 
+end
 end

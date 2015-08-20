3aba6fd1c3ea1e08a6eefb87c83a78b27143301c
wathika <vcwathika@gmail.com>
2015-08-20 16:23:12 +0300

    addde html file with styling

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,7 +1,7 @@
 class Greeter
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello rack"]]
-
+    #[200, {"Content-Type" => "text/plain"}, ["Hello rack"]]
+    Rack::Response.new("Hello")
   end
 
 end

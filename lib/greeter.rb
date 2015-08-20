7c2ae01868ad1ed32da462c04012f9c138e76b82
wathika <vcwathika@gmail.com>
2015-08-20 18:06:08 +0300

    .

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,10 +1,18 @@
 require "erb"
 
 class Greeter
-  def call(env)
+  def self.call(env) #defining call as a class metthod
+    new(env).response.finish
+    #instanciating a new greeter each time(instance variables wont persist btwn requests)
+    #.finish makes it into the array format rack expects
+  end
+  def initialize(env)
     #[200, {"Content-Type" => "text/plain"}, ["Hello rack"]]
     #Rack::Response.new("Hello")
     @request = Rack::Request.new(env)
+  end
+
+  def response
     case @request.path #to access the url path that the user typed in  to access this page
     when "/" then Rack::Response.new(render("index.html.erb"))
     when "/change" #setting a cookie and redirecting back to the root homepage
